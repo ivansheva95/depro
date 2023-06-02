86bf98779d055f6bef88afef2b7d1a5ef795c1d9
IVAN <>
2023-06-02 07:51:37 +0300

    dsf

--- src/components/Cards/SupportCard/SupportCard.tsx
+++ src/components/Cards/SupportCard/SupportCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from './Support.module.scss'
 import Image from 'next/image'
-import { Gold } from '@/components/Svgs'
 
 export default function SupportCard() {
   return (
